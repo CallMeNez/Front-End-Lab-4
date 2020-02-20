4fd1f4be99e8b91fbed444184e1c43562640e7e2
Andy Burt <P17160048@my365.dmu.ac.uk>
2020-02-20 12:46:39 +0000

    finished the lab

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,14 @@
 console.log("hello");
+
+// function setColour() {
+//   theValue.style.backgroundColor = colorPicker.value;
+// }
+
+const menuToggle = document.getElementById("menu-toggle");
+const menuNav = document.getElementById("menu-nav");
+
+function toggleMenu() {
+  menuNav.classList.toggle("menu-nav");
+  console.log("called toggleMenu");
+}
+menuToggle.addEventListener("click", toggleMenu);
